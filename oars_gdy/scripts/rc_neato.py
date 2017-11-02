27aa5a3dc57ff82b463ae7c5d2dd9c2eed4cf50c
jsieving <jane.sieving@students.olin.edu>
2017-11-01 20:58:11 -0400

    started implementing ros

---
+++ oars_gdy/scripts/rc_neato.py
@@ -0,0 +1,27 @@
+import math
+import rospy
+from geometry_msgs.msg import Twist
+
+
+class NeatoRC:
+
+    def __init__(self):
+
+        rospy.init_node('rc_control')
+
+        r = rospy.Rate(5)
+
+        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=0)
+
+
+        self.cmd_vel = Twist()
+        self.cmd_vel.linear.x = 1
+        self.cmd_vel.linear.y = 0
+        self.cmd_vel.linear.z = 0
+
+        while not rospy.is_shutdown():
+
+            self.cmd_pub.publish(self.cmd_vel)
+
+            r.sleep()
+
