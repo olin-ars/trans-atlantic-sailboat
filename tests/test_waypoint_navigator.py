37715a461c4beced81ed460b67d6bd9b392b8606
Noah D'Souza <noah.dsouza@students.olin.edu>
2018-03-04 15:13:51 -0500

    Test for check_distance_to_wp added

--- tests/test_waypoint_navigator.py
+++ tests/test_waypoint_navigator.py
@@ -4,11 +4,12 @@ from oars_gb_pkg.nodes.thinking.waypoint_navigator import WaypointNavigator
 
 class TestWaypointGeneration(unittest.TestCase):
     """
-    Tests the conversion of a list of GridMap cells to navigate into a list of GPS waypoints.
+    Tests the conversion of a list of GridMap
+    cells to navigate into a list of GPS waypoints.
     """
 
     def test_calculate_desired_heading(self):
-        wn = WaypointNavigator()
+        wn = WaypointNavigator(6)
         test_position = Pose2D()
         test_position.x = 1
         test_position.y = 1
@@ -16,6 +17,17 @@ class TestWaypointGeneration(unittest.TestCase):
         wn.update_wp_list([(3,3)],False)
         # Calculate heading
         self.assertEqual(wn.calculate_desired_heading(), 45.0)
+
+    def test_check_distance_to_wp(self):
+        wn = WaypointNavigator(6)
+        test_position = Pose2D()
+        test_position.x = 0
+        test_position.y = 0
+        wn.update_location(test_position,False)
+        wn.update_wp_list([(3,4),(5,12),(7,24)],False)
+        #radius = 6
+        # Calculate heading
+        self.assertEqual(wn.check_distance_to_wp(), True)
 
 
 if __name__ == '__main__':
