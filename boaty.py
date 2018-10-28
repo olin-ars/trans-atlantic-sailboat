4668cf40b357b91385573d42f2cc20c1e09a7184
Shirin Kuppusamy <skuppusamy@olin.edu>
2018-10-28 14:20:13 -0400

    Kyle HAALPPPgit add boaty.py !

--- boaty.py
+++ boaty.py
@@ -3,13 +3,13 @@ import numpy as np
 from short_course import run
 from random import randint
 
-windDir = 0  #wind source direction
+windDir = 90  #wind source direction
 boatCurrDir = 0
 boatDesDir = 0
 targetList = [(200, 0), (0, -200), (-200, 0), (0, 200), (200, 0)]
 wind = Turtle()
 wind.pu()
-wind.setpos(400, 0)
+wind.setpos(250, 0)
 boat = Turtle()
 boat.setheading(boatCurrDir)
 
@@ -36,8 +36,8 @@ def adjustTo(current, goal):
 for targetPos in targetList:
     print("Heading to ", targetPos)
     while boat.distance(targetPos) > 5:
-        windVector = [np.cos(windDir), np.sin(windDir)]
-        boatNewDir = run(boat.position(), targetPos, windVector)
+        #windVector = [np.cos(windDir*np.pi/180), np.sin(windDir*np.pi/180)]
+        boatNewDir = run(boat.position(), boat.heading(), targetPos, windDir)
         #boatCurrDir = adjustTo(boatCurrDir, boatNewDir)
 
         boat.setheading(boatNewDir)
