c4f6e3901594107a35f57f1eb16e2e7f2ac64d6a
Shreya Chowdhary <schowdhary@olin.edu>
2018-11-14 20:40:55 -0500

    More changes to comply with PEP8 style guidelines

--- oars_gb_pkg/helpers/path_planning/short_course_planner.py
+++ oars_gb_pkg/helpers/path_planning/short_course_planner.py
@@ -1,24 +1,26 @@
-'''
+"""
 This program implements the short course routing algorithm described in Section 5.1 of Roland Stelzer's Autonomous
 Sailboat Navigation paper. This algorithm uses a polar efficiency function to determine the efficiency of all possible
 angles that a boat can turn, and then compares the efficiencies to determine the optimal boat heading.
 
 Read the paper here: https://www.dora.dmu.ac.uk/bitstream/handle/2086/7364/thesis-optimized-300dpi.pdf
-'''
+"""
 import numpy as np
+
 
 class ShortCoursePlanner:
 
-    def _get_polar_efficiency(self, wind_angle, alpha):
-        '''
+    @staticmethod
+    def _get_polar_efficiency(wind_angle, alpha):
+        """
         Calculates the hypothetical speed for a given angle alpha, using the polar efficiency function.
 
         :param wind_angle: the wind direction in degrees (relative to 0 degrees, with 0 degrees being east)
         :param alpha: angle being tested (relative to the wind angle)
         :return: the vector components for the velocity for the angle as a numpy array
-        '''
+        """
         # Convert angle to radians
-        angle = (wind_angle + alpha) * np.pi/180
+        angle = (wind_angle + alpha) * np.pi / 180
         alpha *= np.pi / 180
 
         # Calculate magnitude using the polar effeciency function
@@ -28,26 +30,26 @@ class ShortCoursePlanner:
         return np.array([mag * np.cos(angle), mag * np.sin(angle)])
 
     def _get_optima(self, wind_angle, path):
-        '''
+        """
         Finds the maximum velocity and angle that can be achieved for every possible new heading for the boat to
         determine the most optimal headings on the port and starboard sides of the boat.
 
         :param wind: angle representing the wind.
         :param path: vector representing the path taken to reach from boat's position to the target position
         :return: A tuple containing the following information:
-           windangle_max_R - max boat heading on right side
-            windangle_max_L - max boat heading on left side
-            vt_max_R - max velocity on right side
-            vt_max_L - max velocity on left side
-        '''
+           windangle_max_r - max boat heading on right side
+            windangle_max_l - max boat heading on left side
+            vt_max_r - max velocity on right side
+            vt_max_l - max velocity on left side
+        """
 
         # Initializing the right side and left side optimum wind angles
-        windangle_max_R = wind_angle
-        windangle_max_L = wind_angle
+        windangle_max_r = wind_angle
+        windangle_max_l = wind_angle
 
         # Initializing the right side and left side optimum max velocities
-        vt_max_R = 0
-        vt_max_L = 0
+        vt_max_r = 0
+        vt_max_l = 0
 
         # Initializes alpha, or the angle being tested (relative to the wind direction)
         alpha = 0
@@ -60,27 +62,25 @@ class ShortCoursePlanner:
             vb_hyp = self._get_polar_efficiency(wind_angle, alpha)
 
             # Projecting the max speed in the direction of the path
-            vt_test = np.dot(vb_hyp,path)
+            vt_test = np.dot(vb_hyp, path)
 
             # Check if this velocity is the new max
-            if(vt_test>vt_max_R):
-                vt_max_R = vt_test
-                windangle_max_R = (wind_angle + alpha) % 360
+            if vt_test > vt_max_r:
+                vt_max_r = vt_test
+                windangle_max_r = (wind_angle + alpha) % 360
 
         # Analogous to the starboard tack, but for a port tack
-        for alpha in range (-180,0):
+        for alpha in range(-180, 0):
             vb_hyp = self._get_polar_efficiency(wind_angle, alpha)
             vt_test = np.dot(vb_hyp, path)
-            if (vt_test>vt_max_L):
-                vt_max_L = vt_test
-                windangle_max_L = (wind_angle + alpha) % 360
+            if vt_test > vt_max_l:
+                vt_max_l = vt_test
+                windangle_max_l = (wind_angle + alpha) % 360
 
-        return vt_max_R,vt_max_L,windangle_max_R,windangle_max_L
-
-
+        return vt_max_r, vt_max_l, windangle_max_r, windangle_max_l
 
     def _get_best_dir(self, vt_max_R, vt_max_L, windangle_max_R, windangle_max_L, p_c, path, boat_heading):
-        '''
+        """
         Checks whether a port tack or starboard tack would be more efficient.
 
         :param vt_max_R: the maximum velocity on the right hand side
@@ -91,15 +91,15 @@ class ShortCoursePlanner:
         :param path: the path the boat is following as (a numpy array)
         :param boat_heading: the boat's heading, relative to 0 (with 0 as east)
         :return: the new boat direction and speed
-        '''
+        """
 
         # Calculates the hysteresis factor
         n = 1 + (p_c / (abs(np.sqrt(path[0] ** 2 + path[1] ** 2))))
 
         # Choose the direction that is closest to the current boat heading
-        R_diff = 180 - abs(abs(windangle_max_R - boat_heading) - 180);
-        L_diff = 180 - abs(abs(windangle_max_L - boat_heading) - 180);
-        if R_diff < L_diff:
+        r_diff = 180 - abs(abs(windangle_max_R - boat_heading) - 180)
+        l_diff = 180 - abs(abs(windangle_max_L - boat_heading) - 180)
+        if r_diff < l_diff:
             # Choose the faster velocity
             if vt_max_R * n < vt_max_L:
                 new_boat_heading = windangle_max_L
@@ -113,31 +113,32 @@ class ShortCoursePlanner:
 
         return new_boat_heading
 
-    def run(self, Bp, Bh, T, w):
-        '''
+    def run(self, b_p, b_h, t, w):
+        """
         This method directs the boat by using the boat's position and the target position to calculate the path
         and comparing the boat's heading to the wind heading to determine the most efficient way for the boat to get
         to its target position.
 
-        :param Bp - the boat's current position as a Python list
-        :param Bh - the boat's current heading as an angle in degrees
-        :param T - the target position as a Python list
+        :param b_p - the boat's current position as a Python list
+        :param b_h - the boat's current heading as an angle in degrees
+        :param t - the target position as a Python list
         :param w - the wind angle in degrees, relative to 0 (with 0 as east)
         :return new_dir - the boat's new direction as a numpy array
-        '''
+        """
 
         # Calculates the path of the boat
-        path = np.array(T) - np.array(Bp)
+        path = np.array(t) - np.array(b_p)
 
         # The beating parameter (which controls the length of a tack)
         p_c = 20
 
         # Runs the optimum function and compares the optima returned to determine the best direction for
         # boat to go
-        a,b,c,d = self._get_optima(w, path)
-        new_dir = self._get_best_dir(a,b,c,d, p_c, path, Bh)
+        a, b, c, d = self._get_optima(w, path)
+        new_dir = self._get_best_dir(a, b, c, d, p_c, path, b_h)
 
         return new_dir
+
 
 '''
 Runs the sample course provided below and displays the resulting path using Turtle.
